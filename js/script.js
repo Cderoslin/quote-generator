65980565eeef7bc74acb7b699ccc83978b1a6710
Cderoslin <cderoslin@hotmail.com>
2022-04-12 11:15:41 -0400

    Latest updates

--- js/script.js
+++ js/script.js
@@ -7,9 +7,53 @@ Project 4 - Random Quote Generator
   // Check the "Project Resources" section of the project instructions
   // Reach out in your Slack community
 
+  
 
+let quotes = [
+  {
+    quote:"The journey of a thousand miles begins with one step.",
+    source: "- Lao Tzu"
+  },
+  {
+    quote:"You must be the change you wish to see in the world.",
+    source: "- Mahatma Gandhi"
+  },
+  {
+    quote:"If you’re going through hell, keep going.",
+    source: "- Winston Churchill"
+  },
+  {
+    quote:"Life is like a box of chocolates. You never know what you’re going to get.",
+    source: "- Forrest Gump",
+    year: 1994,
+    citation: "Forrest Gump"
+  },
+  {
+    quote:"It always seems impossible until it’s done.",
+    source: "- Nelson Mandela",
+    year: 2001
+  },
+  {
+    quote:"It is never too late to be what you might have been.",
+    source: "- George Eliot",
+    year: 1881
+  },
+  {
+    quote:"Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.",
+    source: "- Bil Keane",
+    year: 1994
+  },
 
-let quotes = [''];
+];
+
+function getRandomQuote(quotes) {
+  let randomNumQuote = Math.floor( Math.random() * quotes.length ) + 1;
+  console.log(5);
+};
+
+// for (let i = 1; i <= 10; i++){
+   
+// }
 
 /*** 
  * `quotes` array 
