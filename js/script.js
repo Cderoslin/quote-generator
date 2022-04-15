1dd4f14d73e4e2a7d932a54ae511d306a63ff7a2
Cderoslin <cderoslin@hotmail.com>
2022-04-14 20:32:52 -0400

    latest updates

--- js/script.js
+++ js/script.js
@@ -9,49 +9,9 @@ Project 4 - Random Quote Generator
 
   
 
-  let quotes = [
-    {
-      quote:"The journey of a thousand miles begins with one step.",
-      source: "- Lao Tzu"
-    },
-    {
-      quote:"You must be the change you wish to see in the world.",
-      source: "- Mahatma Gandhi"
-    },
-    {
-      quote:"If you’re going through hell, keep going.",
-      source: "- Winston Churchill"
-    },
-    {
-      quote:"Life is like a box of chocolates. You never know what you’re going to get.",
-      source: "- Forrest Gump",
-      year: 1994,
-      citation: "Forrest Gump"
-    },
-    {
-      quote:"It always seems impossible until it’s done.",
-      source: "- Nelson Mandela",
-      year: 2001
-    },
-    {
-      quote:"It is never too late to be what you might have been.",
-      source: "- George Eliot",
-      year: 1881
-    },
-    {
-      quote:"Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.",
-      source: "- Bil Keane",
-      year: 1994
-    },
   
-  ];
   
-  function getRandomQuote(array) {
-    let randomNumQuote = Math.floor( Math.random() * array.length );
-    return array[randomNumQuote];
-  };
   
-  console.log(getRandomQuote(quotes));
 
 // for (let i = 1; i <= 10; i++){
    
@@ -61,13 +21,53 @@ Project 4 - Random Quote Generator
  * `` array 
 ***/
 
+let quotes = [
+  {
+    quote:"The journey of a thousand miles begins with one step.",
+    source: "- Lao Tzu"
+  },
+  {
+    quote:"You must be the change you wish to see in the world.",
+    source: "- Mahatma Gandhi"
+  },
+  {
+    quote:"If you’re going through hell, keep going.",
+    source: "- Winston Churchill"
+  },
+  {
+    quote:"Life is like a box of chocolates. You never know what you’re going to get.",
+    source: "- Forrest Gump",
+    year: 1994,
+    citation: "Forrest Gump"
+  },
+  {
+    quote:"It always seems impossible until it’s done.",
+    source: "- Nelson Mandela",
+    year: 2001
+  },
+  {
+    quote:"It is never too late to be what you might have been.",
+    source: "- George Eliot",
+    year: 1881
+  },
+  {
+    quote:"Yesterday is history, tomorrow is a mystery, today is a gift of God, which is why we call it the present.",
+    source: "- Bil Keane",
+    year: 1994
+  },
 
+];
 
 /***
  * `getRandomQuote` function
 ***/
 
+function getRandomQuote(array) {
+  let randomNumQuote = Math.floor( Math.random() * array.length );
+  return array[randomNumQuote];
+};
 
+console.log(getRandomQuote(quotes[quote]));
 
 /***
  * `printQuote` function
